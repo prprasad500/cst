4252ad6f49f640db2bdb954bf05a4bc6bf9b6808
Lenovo <Lenovo@DESKTOP-26EJCSA>
2022-03-15 20:41:18 +0530

    first

---
+++ jdp/src/main/java/com/slokam/java/jdp/JdpApplication.java
@@ -0,0 +1,14 @@
+package com.slokam.java.jdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdpApplication {
+
+	public static void main(String[] args) {
+		//
+		SpringApplication.run(JdpApplication.class, args);
+	}
+
+}
