4041e41c74d08107a7cac32ce9c674ab1ee4be90
Lenovo <Lenovo@DESKTOP-26EJCSA>
2022-03-18 12:52:28 +0530

    commeted files

---
+++ jdp/src/test/java/com/slokam/java/jdp/JdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slokam.java.jdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
